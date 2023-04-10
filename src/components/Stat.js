1bf64d107693b1eba0c010ea7989c63a02a889b8
Phil J Gray <phl.j.gray@gmail.com>
2023-04-10 00:18:23 -0700

    check grid

--- src/components/Stat.js
+++ src/components/Stat.js
@@ -2,18 +2,19 @@
 import data from '../data.json'
 
 
-export default function Stat({ title }) {
+export default function Stat() {
+
     const statDetails = data.map(detail =>
-        <div className="container-tab">
+        <div className={`container-tab ${detail.title}`}>
             <img className="svg-style" alt={detail.title}></img>
             <section className="section-stats flex">
                 <div className="current-stat">
                     <h1>{detail.title}</h1>
-                    <h1>Current {detail.timeframes.current}</h1>
+                    <h1>Current {detail.timeframes.daily.current}</h1>
                 </div>
                 <div className="last-week-stat">
                     {/* <img src={svg}></img> */}
-                    <p>Last Week </p>
+                    <p>Last Week {detail.timeframes.daily.current}</p>
                 </div>
             </section>
         </div>
